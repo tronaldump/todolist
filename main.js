b58221ea629ad95e8cfd66b34e00228a9d232596
Keith Bryant <kabryant@cinci.rr.com>
2017-06-08 16:00:16 -0400

    to_do_list

--- main.js
+++ main.js
@@ -1,1 +1,40 @@
 // Add your JS here.
+
+
+
+
+var element = document.querySelector('.todo-list');
+element.classList.add('li');
+
+var element = document.querySelector('.todo-list');
+var listItem = document.createElement('li');
+listItem.textContent = 'Sweep';
+element.appendChild(Sweep);
+
+var element = document.querySelector('.todo-list');
+var listItem = document.createElement('li');
+listItem.textContent = 'Mop';
+element.appendChild('Mop');
+
+var element = document.querySelector('.todo-list');
+var listItem = document.createElement('li');
+listItem.textContent = 'Dishes';
+element.appendChild('Dishes');
+
+var element = document.querySelector('.todo-list');
+var listItem = document.createElement('li');
+listItem.textContent = 'Windows';
+element.appendChild('Windows');
+
+var element = document.querySelector('.todo-list');
+var listItem = document.createElement('li');
+listItem.textContent = 'Dogs';
+element.appendChild('Dogs');
+
+var .new-todoElement = document.getElementById(.new-todoElement)
+
+.new-todoElement.addEventListener("return", ourCallBack);
+
+function ourCallBack() {
+  .new-todoElement
+}
